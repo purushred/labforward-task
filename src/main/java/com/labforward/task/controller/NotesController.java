0946befd07c0a7b46c08daf5772e14454f9c9426
Purushothama Reddy YB <purushothama-reddy.yb@hpe.com>
2021-05-30 09:52:24 +0530

    Commented localhost URL for development

--- src/main/java/com/labforward/task/controller/NotesController.java
+++ src/main/java/com/labforward/task/controller/NotesController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,9 +20,10 @@ import com.labforward.task.service.interfaces.INotesService;
  */
 
 @RequestMapping(value = { "/api/v1/lab-notes" })
-
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+
+// Uncomment this line for local development testing.
+//@CrossOrigin(origins = "http://localhost:4200")
 public class NotesController {
 
     @Autowired
